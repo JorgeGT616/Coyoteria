918d7d77dbfb7ff99f6722bf846334c99ccbc659
Naimad Gam <raul3e@comunidad.unam.mx>
2020-05-29 11:17:19 -0500

    FEAT: Contraseñas cifradas

--- dynamics/revision.php
+++ dynamics/revision.php
@@ -8,6 +8,58 @@
   </head>
   <body>
     <?php
+      define("HASH","sha256");
+      define("PASSWORD","Secure password, plz make ec¿verything s3cur3");
+      define("METHOD","aes-128-cbc");
+
+      function Cifrar($text)
+      {
+        $key = openssl_digest(PASSWORD,HASH);
+        $iv_len = openssl_cipher_iv_length(METHOD);
+
+        $iv = openssl_random_pseudo_bytes($iv_len);
+        $textoCifrado = openssl_encrypt
+        (
+          $text,
+          METHOD,
+          $key,
+          OPENSSL_RAW_DATA,
+          $iv
+        );
+        $ciffWIv=base64_encode($iv.$textoCifrado);
+        return $ciffWIv;
+      }
+
+      function Descifrar($cifradoWIv)
+      {
+        $cifradoWIv=base64_decode($cifradoWIv);
+        $iv_len= openssl_cipher_iv_length(METHOD);
+        $iv= substr($cifradoWIv,0,$iv_len);
+        $cifrado = substr($cifradoWIv,$iv_len);
+
+        $key=openssl_digest(PASSWORD,HASH);
+
+        $desciff=openssl_decrypt
+        (
+          $cifrado,
+          METHOD,
+          $key,
+          OPENSSL_RAW_DATA,
+          $iv
+        );
+
+        return $desciff;
+      }
+      /*$mensaje = "Pio pio, por favor no me peguen xd";
+      $ciff = Cifrar($mensaje);
+      //$textoOriginal=Descifrar(Cifrar("textoOriginal"));
+      $desciff = Descifrar($ciff);
+      //echo $desciff;
+
+      echo "Mensaje Original: ".$mensaje."<br>";
+      echo "Mensaje Cifrado: ".$ciff."<br>";
+      echo "Mensaje Descifrado: ".$desciff."<br>";*/
+
       if (isset($_POST['contrasena']) && $_POST['contrasena'] != "") {
         $contrasenaprin = (isset($_POST['contrasena']) && $_POST['contrasena'] != "") ? $_POST['contrasena'] : 'Coloque contraseña' ;
 
@@ -74,6 +126,7 @@
               }
               if (isset($_POST['contrasena']) && $_POST['contrasena'] != "") {
                 $pass = $_POST['contrasena'];
+                $pass = Cifrar($pass);
               }
               if (isset($numcuenta) && isset($pass)) {
                 $nombre.=" ".$papellido." ".$mapellido;
@@ -92,6 +145,7 @@
               }
               if (isset($_POST['contrasena']) && $_POST['contrasena'] != "") {
                 $pass = $_POST['contrasena'];
+                $pass = Cifrar($pass);
               }
               if (isset($rfc) && isset($pass)) {
                 $nombre.=" ".$papellido." ".$mapellido;
@@ -109,6 +163,7 @@
               }
               if (isset($_POST['contrasena']) && $_POST['contrasena'] != "") {
                 $pass = $_POST['contrasena'];
+                $pass = Cifrar($pass);
               }
               if (isset($numtrab) && isset($pass)) {
                 $nombre.=" ".$papellido." ".$mapellido;
